1375cf13484ffbcf3dca939a8659925477a2d6a0
Jesse Gumm <gumm@sigma-star.com>
2020-09-20 15:07:05 -0500

    13.4

--- site/src/index.erl
+++ site/src/index.erl
@@ -11,12 +11,30 @@ title() ->
 body_left() ->
     [
      #label{text="Stock Symbol"},
-     #textbox{id=symbol},
+     #textbox{id=symbol, class='form-control'},
      #br{},
      #button{
         text="Show Quotes",
-        postback=get_quotes
-       }
+        postback=get_quotes,
+        class=[btn,'btn-success']
+     }
     ].
 
+body_right() ->
+    #panel{id=quotes}.
 
+event(get_quotes) ->
+    Symbol = wf:q(symbol),
+    get_and_insert_quote(Symbol).
+
+get_and_insert_quote(Symbol) ->
+    Quote = stock:lookup(Symbol),
+    QuoteTime = qdate:to_string("g:i:sa"),
+    Body = #panel{text=[
+        Symbol,
+        " (",
+        QuoteTime,
+        "): ",
+        Quote
+    ]},
+    wf:insert_top(quotes, Body).
