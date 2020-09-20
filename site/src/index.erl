c004a900b41b18cd4d12768e1f04fe69f2652af7
Jesse Gumm <gumm@sigma-star.com>
2020-09-20 13:01:15 -0500

    13.3.4

--- site/src/index.erl
+++ site/src/index.erl
@@ -1,39 +1,22 @@
-%% -*- mode: nitrogen -*-
--module (index).
--compile(export_all).
+-module(index).
 -include_lib("nitrogen_core/include/wf.hrl").
+-compile(export_all).
 
-main() -> #template { file="./site/templates/bare.html" }.
+main() ->
+    #template{file="site/templates/bare.html"}.
 
-title() -> "Welcome to Nitrogen".
+title() ->
+    "Home".
 
-body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
-
-inner_body() -> 
+body_left() ->
     [
-        #h1 { text="Welcome to Nitrogen" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-		#p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        ",
-		#p{},
-		"
-		<b>Want to see the ",#link{text="Sample Nitrogen jQuery Mobile Page",url="/mobile"},"?</b>
-		"
+     #label{text="Stock Symbol"},
+     #textbox{id=symbol},
+     #br{},
+     #button{
+        text="Show Quotes",
+        postback=get_quotes
+       }
     ].
-	
-event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
-    }).
+
+
---
+++ site/src/st_common.erl
@@ -0,0 +1,5 @@
+-module(st_common).
+-include_lib("nitrogen_core/include/wf.hrl").
+-compile(export_all).
+
+menu() -> [].
