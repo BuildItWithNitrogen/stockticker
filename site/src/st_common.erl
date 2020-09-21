a72684e3dee3bbfb28855b3ac4f610f95f0f589f
Jesse Gumm <gumm@sigma-star.com>
2020-09-20 19:31:19 -0500

    13.5.1

--- site/src/st_common.erl
+++ site/src/st_common.erl
@@ -2,7 +2,19 @@
 -include_lib("nitrogen_core/include/wf.hrl").
 -export([favorites/0, menu/0]).
 
-menu() -> [].
+menu() ->
+    Items = menu_items(),
+    menu_bind(Items).
+
+menu_bind(Items) ->
+    Map = {menu_link@text, menu_link@url},
+    #list{class='navbar-nav', body=[
+        #bind{data=Items, map=Map, body=[
+            #listitem{class='nav-item', body=[
+                #link{class='nav-link', id=menu_link}
+            ]}
+        ]}
+    ]}.
 
 menu_items() ->
     [
